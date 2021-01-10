9f0cf9a4ce7cddd051754e6036105950fb6d3519
Zealot <51274537+aZealot@users.noreply.github.com>
2021-01-10 17:59:33 -0600

    Update script.js

--- snek/script.js
+++ snek/script.js
@@ -4,4 +4,12 @@ var ctx = gameWindow.getContext("2d");
 var snekTiles = new Image();
 snekTiles.src = "https://azealot.github.io/snek/snek.png";
 
-ctx.drawImage(snekTiles,16,16);
+//
+
+var fps = 4;
+function animate() {
+setTimeout(function() {
+  requestAnimationFrame(animate);
+  ctx.drawImage(snekTiles,16,16);
+}, 1000 / framesPerSecond);
+}
