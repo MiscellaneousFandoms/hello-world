dd92b110c8cd232cd5d133a3b9866e3ab14c1d40
Zealot <51274537+aZealot@users.noreply.github.com>
2021-01-09 18:30:45 -0600

    Update script.js

--- snek/script.js
+++ snek/script.js
@@ -2,6 +2,6 @@ var gameWindow = document.getElementById("gameWindow");
 var ctx = gameWindow.getContext("2d");
 
 var snekTiles = new Image();
-snekTiles.src = "https://azealot.github.io/snek/snek.png";
+snekTiles.src = "/snek.png";
 
 ctx.drawImage(snekTiles,16,16);
