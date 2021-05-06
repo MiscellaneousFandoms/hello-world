8010190cb7ef1eca51a124955865e6780016bf97
Zealot <51274537+aZealot@users.noreply.github.com>
2021-05-06 16:18:18 -0500

    Delete Windows 1 Font directory

--- Windows 1 Font/script.js
+++
@@ -1,8 +0,0 @@
-text = document.getElementById('text');
-text.value =
-' !"#$%&\'()*+,-./\n'+
-'0123456789:;<=>?\n'+
-'@ABCDEFGHIJKLMNO\n'+
-'PQRSTUVWXYZ[\\]^_\n'+
-'`abcdefghijklmno\n'+
-'pqrstuvwxyz{|}~ \n';
