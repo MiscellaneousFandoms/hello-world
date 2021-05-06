7fd3c480d5936beaf90bed495964cf244d2747f4
Zealot <51274537+aZealot@users.noreply.github.com>
2021-05-06 16:24:42 -0500

    Add files via upload

---
+++ windows1font/script.js
@@ -0,0 +1,8 @@
+text = document.getElementById('text');
+text.value =
+' !"#$%&\'()*+,-./\n'+
+'0123456789:;<=>?\n'+
+'@ABCDEFGHIJKLMNO\n'+
+'PQRSTUVWXYZ[\\]^_\n'+
+'`abcdefghijklmno\n'+
+'pqrstuvwxyz{|}~ \n';
