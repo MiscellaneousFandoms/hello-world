7204f1c699c6654b56b92d5ed41e2ff301a18953
aZealot <51274537+aZealot@users.noreply.github.com>
2021-11-10 20:33:40 -0600

    Delete snek directory

--- snek/script.js
+++
@@ -1,9 +0,0 @@
-var gameWindow = document.getElementById("gameWindow");
-var ctx = gameWindow.getContext("2d");
-
-var snekTiles = new Image();
-snekTiles.src = "https://azealot.github.io/snek/snek.png";
-
-//
-
-ctx.drawImage(snekTiles,16,16);
