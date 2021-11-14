591e9e0d4394005eff57278e31826951a74c6429
aZealot <51274537+aZealot@users.noreply.github.com>
2021-11-13 23:27:39 -0600

    Add files via upload

---
+++ 404/script.js
@@ -0,0 +1,10 @@
+const text = document.getElementById("text");
+text.focus();
+let content = text.innerHTML;
+text.value = null;
+text.value = content;
+
+function resize() {
+  text.style.height = "";
+  text.style.height = text.scrollHeight + "px";
+}
